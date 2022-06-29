307facfef5dd7feabd63371c81233f859c4ddd6d
iamoscarb <63667410+iamoscarb@users.noreply.github.com>
2022-06-28 19:17:00 -0500

    update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,13 +1,20 @@
 var express = require('express');
 app = express();
+var response;
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
 app.get('/mars', function (req, res) {
+  
   res.send('Hello Mars!\n');
-});
+});*/
+
+app.get('/',function(req, res){
+  response = "Esta es la segunda version de la app c:\n";
+  res.send(response);
+}
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
