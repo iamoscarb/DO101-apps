dc039ae0037a75c8b9b8a9a1ad27a20519efe44d
iamoscarb <oscaribh97@gmail.com>
2022-06-28 20:44:23 -0500

    Entrar

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 app = express();
+var response;
+
+/*app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});*/
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  response='Esta es la segunda version de la app\n'
+  res.send(response);
 });
 
 app.listen(8080, function () {
